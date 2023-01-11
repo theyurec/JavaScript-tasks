e3894cce422810330f1b5cecfe2266f2e04c6b42
Yuriy <the.yurec1@yandex.ru>
2023-01-11 13:04:40 +0300

    task list

--- tasksList-7.js
+++ tasksList-7.js
@@ -7,3 +7,37 @@
 	- Перенос задачи в начало списка по названию 
 	Всегда меняем исходный массив
 */
+
+const tasks = ['Задача 1'];
+
+function addTask (task) {
+	tasks.push(task)
+	
+}
+
+function deleteTask (task) {
+	const deleteEl = tasks.indexOf(task)
+	if(deleteEl !== -1) {
+		tasks.splice(deleteEl, 1)
+	}
+	
+}
+
+function addTaskStart (task) {
+	const findEl = tasks.indexOf(task)
+	if(findEl !== -1) {
+		tasks.unshift(...tasks.splice(findEl, 1))
+	}
+}
+
+addTask ('Задача 2')
+addTask ('Задача 3')
+console.log(tasks)
+
+deleteTask('Задача 3')
+console.log(tasks)
+
+addTaskStart('Задача 2')
+console.log(tasks)
+
+
